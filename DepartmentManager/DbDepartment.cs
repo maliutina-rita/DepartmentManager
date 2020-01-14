5b65e73759b72089bd54904c3095272b3ead460f
maliutina-margarita <maliutina.margarita@gmail.com>
2020-01-14 14:49:07 +0300

    setup project

---
+++ DepartmentManager/DbDepartment.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DepartmentManager
+{
+    [Table("Departament")]
+    public class DbDepartment
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
